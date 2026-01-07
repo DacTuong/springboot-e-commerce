b2fc94513174260be869fcf2abaa1735b510ed01
DacTuong <dactuong13@gmail.com>
2026-01-07 16:52:59 +0700

    Add the webconfig file to display images located in the uploads/brand folder.

--- src/main/java/springboot_ecommerce/springboot_ecommerce/config/Webconfig.java
+++ src/main/java/springboot_ecommerce/springboot_ecommerce/config/Webconfig.java
@@ -1,13 +1,15 @@
 package springboot_ecommerce.springboot_ecommerce.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations(
+                        "file:" + System.getProperty("user.dir") + "/uploads/");
     }
 }
